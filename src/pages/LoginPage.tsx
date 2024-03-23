c8acfc54d6dfcf412ad39622d30e6c74d46579e8
firatgoktepe <firatgoktepe1986@gmail.com>
2024-03-23 23:19:25 +0300

    add: loginpage created

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import LoginForm from "../components/LoginForm";
+
+const LoginPage: React.FC = () => {
+  const { isLoggedIn } = useAuth();
+
+  if (isLoggedIn) {
+    return <Navigate to="/" replace />;
+  }
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;
