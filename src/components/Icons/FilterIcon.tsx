9583d07d959aa7d436d7f72c191998ea3f629549
firatgoktepe <firatgoktepe1986@gmail.com>
2024-03-24 15:07:16 +0300

    add: filter & sort icons

---
+++ src/components/Icons/FilterIcon.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const FilterIcon: React.FC = () => {
+  return (
+    <svg
+      width="18"
+      height="19"
+      viewBox="0 0 18 19"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M9 3.5V1.5M9 3.5C7.89543 3.5 7 4.39543 7 5.5C7 6.60457 7.89543 7.5 9 7.5M9 3.5C10.1046 3.5 11 4.39543 11 5.5C11 6.60457 10.1046 7.5 9 7.5M3 15.5C4.10457 15.5 5 14.6046 5 13.5C5 12.3954 4.10457 11.5 3 11.5M3 15.5C1.89543 15.5 1 14.6046 1 13.5C1 12.3954 1.89543 11.5 3 11.5M3 15.5V17.5M3 11.5V1.5M9 7.5V17.5M15 15.5C16.1046 15.5 17 14.6046 17 13.5C17 12.3954 16.1046 11.5 15 11.5M15 15.5C13.8954 15.5 13 14.6046 13 13.5C13 12.3954 13.8954 11.5 15 11.5M15 15.5V17.5M15 11.5V1.5"
+        stroke="#0EA5E9"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default FilterIcon;
---
+++ src/components/Icons/SortIcon.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const SortIcon: React.FC = () => {
+  return (
+    <svg
+      width="20"
+      height="19"
+      viewBox="0 0 20 19"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M5 13.5V1.5M5 1.5L1 5.5M5 1.5L9 5.5M15 5.5V17.5M15 17.5L19 13.5M15 17.5L11 13.5"
+        stroke="#0EA5E9"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default SortIcon;
