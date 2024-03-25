c2cdee6bbe74cc04e669bd75daf71cab1389f468
firatgoktepe <firatgoktepe1986@gmail.com>
2024-03-25 13:52:36 +0300

    feat: chevronRight comp created

---
+++ src/components/Icons/ChevronRight.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const ChevronRight: React.FC = () => {
+  return (
+    <svg
+      width="15"
+      height="15"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M7.5 4.16668L13.3333 10L7.5 15.8333"
+        stroke="#2196F3"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default ChevronRight;
